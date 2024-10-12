a4dfeda8f12b48f11e1ab5fae6c4e928053f3409
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2024-10-12 14:24:05 +0530

    Time: 5 ms (31.78%), Space: 10.9 MB (92.86%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,52 +1,50 @@
 class Solution {
-public:
-    double dfs(string start, string end, unordered_map<string, unordered_map<string, double>>& graph, unordered_set<string>& visited){
-        if(graph.find(start) == graph.end()){
-            return -1;
-        }
-        
-        if(graph[start].find(end) != graph[start].end()){
-            return graph[start][end];
-        }
-        
-        for(const auto& nei : graph[start]){
-            if(find(visited.begin(), visited.end(), nei.first) != visited.end())
-                continue;
-            visited.insert(nei.first);
-            double res;
-            if((res = dfs(nei.first, end, graph, visited)) != -1){
-                //optimization: runtime 4ms -> 0ms
-                return graph[start][end] = graph[start][nei.first] * res;
-            }
-        }
-        
-        return -1;
+ public:
+  vector<double> calcEquation(vector<vector<string>>& equations,
+                              vector<double>& values,
+                              vector<vector<string>>& queries) {
+    vector<double> ans;
+    // graph[A][B] := A / B
+    unordered_map<string, unordered_map<string, double>> graph;
+
+    for (int i = 0; i < equations.size(); ++i) {
+      const string& A = equations[i][0];
+      const string& B = equations[i][1];
+      graph[A][B] = values[i];
+      graph[B][A] = 1 / values[i];
     }
-    
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, unordered_map<string, double>> quot;
-        
-        int N = equations.size();
-        
-        for(int i = 0; i < N; i++){
-            const vector<string>& equation = equations[i];
-            const string& num = equation[0], den = equation[1];
-            const double& value = values[i];
-            quot[num][num] = 1.0;
-            quot[den][den] = 1.0;
-            quot[num][den] = value;
-            quot[den][num] = 1.0/value;
-        }
-        
-        vector<double> ans;
-        unordered_set<string> visited;
-        
-        for(const vector<string>& query : queries){
-            const string& num = query[0], den = query[1];
-            visited = {num};
-            ans.push_back(dfs(num, den, quot, visited));
-        }
-        
-        return ans;
+
+    for (const vector<string>& query : queries) {
+      const string& A = query[0];
+      const string& C = query[1];
+      if (!graph.contains(A) || !graph.contains(C))
+        ans.push_back(-1);
+      else
+        ans.push_back(divide(graph, A, C, unordered_set<string>()));
     }
+
+    return ans;
+  }
+
+ private:
+  // Returns A / C.
+  double divide(
+      const unordered_map<string, unordered_map<string, double>>& graph,
+      const string& A, const string& C, unordered_set<string>&& seen) {
+    if (A == C)
+      return 1.0;
+
+    seen.insert(A);
+
+    // value := A / B
+    for (const auto& [B, value] : graph.at(A)) {
+      if (seen.contains(B))
+        continue;
+      const double res = divide(graph, B, C, std::move(seen));  // B / C
+      if (res > 0)                                              // valid result
+        return value * res;  // A / C = (A / B) * (B / C)
+    }
+
+    return -1;  // invalid result
+  }
 };
