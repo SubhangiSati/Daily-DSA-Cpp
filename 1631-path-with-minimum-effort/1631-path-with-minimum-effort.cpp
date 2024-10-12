6faa0fb32fe95e44535eb4795f57f9181d596386
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2024-10-12 14:27:30 +0530

    Time: 128 ms (25.85%), Space: 25.9 MB (27.89%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,46 +1,29 @@
-struct T {
-  int i;
-  int j;
-  int d;  // the maximum difference of (i, j) and its neighbors
-};
-
 class Solution {
- public:
-  int minimumEffortPath(vector<vector<int>>& heights) {
-    constexpr int dirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    const int m = heights.size();
-    const int n = heights[0].size();
-    auto compare = [](const T& a, const T& b) { return a.d > b.d; };
-    priority_queue<T, vector<T>, decltype(compare)> minHeap(compare);
-    // diff[i][j] := the maximum absolute difference to reach (i, j)
-    vector<vector<int>> diff(m, vector<int>(n, INT_MAX));
-    vector<vector<bool>> seen(m, vector<bool>(n));
+public:
+  int minimumEffortPath(vector<vector<int>> &heights) {
+    int m = heights.size(), n = heights[0].size();
+    int dirs[5] = {-1, 0, 1, 0, -1};
 
-    minHeap.emplace(0, 0, 0);
-    diff[0][0] = 0;
+    std::vector<vector<int>> efforts(m, vector<int>(n, INT_MAX));
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    while (!minHeap.empty()) {
-      const auto [i, j, d] = minHeap.top();
-      minHeap.pop();
-      if (i == m - 1 && j == n - 1)
-        return d;
-      seen[i][j] = true;
-      for (const auto& [dx, dy] : dirs) {
-        const int x = i + dx;
-        const int y = j + dy;
-        if (x < 0 || x == m || y < 0 || y == n)
-          continue;
-        if (seen[x][y])
-          continue;
-        const int newDiff = abs(heights[i][j] - heights[x][y]);
-        const int maxDiff = max(diff[i][j], newDiff);
-        if (diff[x][y] > maxDiff) {
-          diff[x][y] = maxDiff;
-          minHeap.emplace(x, y, maxDiff);
-        }
+    pq.emplace(0, 0); // First item is effort, second is row * 100 + col
+    while (!pq.empty()) {
+      int effort = pq.top().first;
+      int x = pq.top().second / 100, y = pq.top().second % 100;
+      pq.pop();
+
+      if (x == m - 1 && y == n - 1) return effort;
+      if (effort >= efforts[x][y]) continue;
+      efforts[x][y] = effort;
+
+      for (int i = 0; i < 4; i++) {
+        int nx = x + dirs[i], ny = y + dirs[i + 1];
+        if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+        int n_effort = max(effort, abs(heights[x][y] - heights[nx][ny]));
+        pq.emplace(n_effort, nx * 100 + ny);
       }
     }
-
-    throw;
+    return -1;
   }
 };
