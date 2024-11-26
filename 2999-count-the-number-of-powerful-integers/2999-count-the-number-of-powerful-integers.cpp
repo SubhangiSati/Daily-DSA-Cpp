a5a0cea81b741a19091b08a095c3772c68e80cc6
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2024-11-26 13:01:26 +0530

    Time: 1 ms (65.92%), Space: 10.4 MB (57.03%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
@@ -1,49 +1,35 @@
 class Solution {
- public:
-  long long numberOfPowerfulInt(long long start, long long finish, int limit,
-                                string s) {
-    const string a = to_string(start);
-    const string b = to_string(finish);
-    const string aWithLeadingZeros = string(b.length() - a.length(), '0') + a;
-    vector<vector<vector<long>>> mem(
-        b.length(), vector<vector<long>>(2, vector<long>(2, -1)));
-    const string sWithLeadingZeros = string(b.length() - s.length(), '0') + s;
-    return count(aWithLeadingZeros, b, 0, limit, s, true, true, mem);
-  }
+public:
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
+        string t = to_string(start - 1);
+        long long f[20];
+        memset(f, -1, sizeof(f));
 
- private:
-  // Returns the number of powerful integers, considering the i-th digit, where
-  // `isTight1` indicates if the current digit is tightly bound for `a` and
-  // `isTight2` indicates if the current digit is tightly bound for `b`.
-  long count(const string& a, const string& b, int i, int limit,
-             const string& s, bool isTight1, bool isTight2,
-             vector<vector<vector<long>>>& mem) {
-    if (i + s.length() == b.length()) {
-      const string aMinSuffix = isTight1
-                                    ? std::string(a.end() - s.length(), a.end())
-                                    : string(s.length(), '0');
-      const string bMaxSuffix = isTight2
-                                    ? std::string(b.end() - s.length(), b.end())
-                                    : string(s.length(), '9');
-      const long suffix = stoll(s);
-      return stoll(aMinSuffix) <= suffix && suffix <= stoll(bMaxSuffix);
+        function<long long(int, bool)> dfs = [&](int pos, bool lim) -> long long {
+            if (t.size() < s.size()) {
+                return 0;
+            }
+            if (!lim && f[pos] != -1) {
+                return f[pos];
+            }
+            if (t.size() - pos == s.size()) {
+                return lim ? s <= t.substr(pos) : 1;
+            }
+            long long ans = 0;
+            int up = min(lim ? t[pos] - '0' : 9, limit);
+            for (int i = 0; i <= up; ++i) {
+                ans += dfs(pos + 1, lim && i == (t[pos] - '0'));
+            }
+            if (!lim) {
+                f[pos] = ans;
+            }
+            return ans;
+        };
+
+        long long a = dfs(0, true);
+        t = to_string(finish);
+        memset(f, -1, sizeof(f));
+        long long b = dfs(0, true);
+        return b - a;
     }
-
-    if (mem[i][isTight1][isTight2] != -1)
-      return mem[i][isTight1][isTight2];
-
-    long res = 0;
-    const int minDigit = isTight1 ? a[i] - '0' : 0;
-    const int maxDigit = isTight2 ? b[i] - '0' : 9;
-
-    for (int d = minDigit; d <= maxDigit; ++d) {
-      if (d > limit)
-        continue;
-      const bool nextIsTight1 = isTight1 && (d == minDigit);
-      const bool nextIsTight2 = isTight2 && (d == maxDigit);
-      res += count(a, b, i + 1, limit, s, nextIsTight1, nextIsTight2, mem);
-    }
-
-    return mem[i][isTight1][isTight2] = res;
-  }
 };
