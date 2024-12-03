6682ed3d92c3c769c1e7b1382f5226d3827c3f6b
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2024-12-03 22:06:23 +0530

    Time: 25 ms (25.90%), Space: 85.6 MB (16.63%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    string addSpaces(string s, vector<int>& A) {
-        string ans;
-        ans.reserve(s.size() + A.size()); // Optional: pre-allocate enough space for the answer.
-        int N = s.size(), j = 0, M = A.size(); // `A[j]` is the next index before which a space should be inserted
-        for (int i = 0; i < N; ++i) {
-            if (j < M && i == A[j]) { // If `i == A[j]`, we insert a space
+    string addSpaces(string s, vector<int>& spaces) {
+        string ans = "";
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            if (j < spaces.size() && i == spaces[j]) {
                 ans += ' ';
                 ++j;
             }
