b2a27debb8e265411dce2a2a562637695c8bad94
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-07-14 16:50:04 +0530

    Time: 13 ms (100.00%) | Memory: 46.7 MB (94.44%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.java
@@ -1,24 +1,86 @@
 class Solution {
-  public int subsequencePairCount(int[] nums) {
-    final int maxNum = Arrays.stream(nums).max().getAsInt();
-    Integer[][][] mem = new Integer[nums.length][maxNum + 1][maxNum + 1];
-    return subsequencePairCount(nums, 0, 0, 0, mem);
-  }
+    private static final int MOD = 1_000_000_007;
+    private static final int MX = 201;
 
-  private static final int MOD = 1_000_000_007;
+    private static final int[][] lcms = new int[MX][MX];
+    private static final int[] pow2 = new int[MX];
+    private static final int[] pow3 = new int[MX];
+    private static final int[] mu = new int[MX];
+    private static boolean initialized = false;
 
-  private int subsequencePairCount(int[] nums, int i, int x, int y, Integer[][][] mem) {
-    if (i == nums.length)
-      return (x > 0 && x == y) ? 1 : 0;
-    if (mem[i][x][y] != null)
-      return mem[i][x][y];
-    final int skip = subsequencePairCount(nums, i + 1, x, y, mem);
-    final int take1 = subsequencePairCount(nums, i + 1, gcd(x, nums[i]), y, mem);
-    final int take2 = subsequencePairCount(nums, i + 1, x, gcd(y, nums[i]), mem);
-    return mem[i][x][y] = (int) (((long) skip + take1 + take2) % MOD);
-  }
+    public Solution() {
+        if (initialized) {
+            return;
+        }
+        initialized = true;
 
-  private int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-  }
+        for (int i = 1; i < MX; i++) {
+            for (int j = 1; j < MX; j++) {
+                lcms[i][j] = lcm(i, j);
+            }
+        }
+
+        pow2[0] = pow3[0] = 1;
+        for (int i = 1; i < MX; i++) {
+            pow2[i] = pow2[i - 1] * 2 % MOD;
+            pow3[i] = (int) ((long) pow3[i - 1] * 3 % MOD);
+        }
+
+        mu[1] = 1;
+        for (int i = 1; i < MX; i++) {
+            for (int j = i * 2; j < MX; j += i) {
+                mu[j] -= mu[i];
+            }
+        }
+    }
+
+    public int subsequencePairCount(int[] nums) {
+        int m = 0;
+        for (int x : nums) {
+            m = Math.max(m, x);
+        }
+        int[] cnt = new int[m + 1];
+        for (int x : nums) {
+            cnt[x]++;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] += cnt[j]; 
+            }
+        }
+
+        int[][] f = new int[m + 1][m + 1];
+        for (int g1 = 1; g1 <= m; g1++) {
+            for (int g2 = 1; g2 <= m; g2++) {
+                int l = lcms[g1][g2];
+                int c = l <= m ? cnt[l] : 0;
+                int c1 = cnt[g1];
+                int c2 = cnt[g2];
+                f[g1][g2] = (int) (((long) pow3[c] * pow2[c1 + c2 - c * 2] - pow2[c1] - pow2[c2] + 1) % MOD);
+            }
+        }
+
+        long ans = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= m / i; j++) {
+                for (int k = 1; k <= m / i; k++) {
+                    ans += mu[j] * mu[k] * f[j * i][k * i];
+                }
+            }
+        }
+        return (int) ((ans % MOD + MOD) % MOD); 
+    }
+
+    private static int gcd(int a, int b) {
+        while (a != 0) {
+            int tmp = a;
+            a = b % a;
+            b = tmp;
+        }
+        return b;
+    }
+
+    private static int lcm(int a, int b) {
+        return a / gcd(a, b) * b;
+    }
 }
