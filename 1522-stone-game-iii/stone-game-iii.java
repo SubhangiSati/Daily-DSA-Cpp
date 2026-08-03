a7ab31a85348dfd757cec9422e5259ea6c97008f
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-08-03 12:53:56 +0530

    Time: 2 ms (100.00%) | Memory: 80.9 MB (99.15%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,33 +1,21 @@
-public class Solution {
-    private Integer[][] dp;
-    private int n;
-
+class Solution {
     public String stoneGameIII(int[] stoneValue) {
-        n = stoneValue.length;
-        dp = new Integer[n][2];
-
-        int result = dfs(0, 1, stoneValue);
-        if (result == 0) return "Tie";
-        return result > 0 ? "Alice" : "Bob";
-    }
-
-    private int dfs(int i, int alice, int[] stoneValue) {
-        if (i >= n) return 0;
-        if (dp[i][alice] != null) return dp[i][alice];
-
-        int res = alice == 1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        int score = 0;
-        for (int j = i; j < Math.min(i + 3, n); j++) {
-            if (alice == 1) {
-                score += stoneValue[j];
-                res = Math.max(res, score + dfs(j + 1, 0, stoneValue));
-            } else {
-                score -= stoneValue[j];
-                res = Math.min(res, score + dfs(j + 1, 1, stoneValue));
-            }
+        int sufSum = 0;
+        int f1 = 0;
+        int f2 = 0;
+        int f3 = 0;
+        for (int i = stoneValue.length - 1; i >= 0; i--) {
+            sufSum += stoneValue[i];
+            int newF = sufSum - Math.min(Math.min(f1, f2), f3);
+            f3 = f2;
+            f2 = f1;
+            f1 = newF;
         }
 
-        dp[i][alice] = res;
-        return res;
+        int diff = f1 - (sufSum - f1);
+        if (diff == 0) {
+            return "Tie";
+        }
+        return diff > 0 ? "Alice" : "Bob";
     }
 }
