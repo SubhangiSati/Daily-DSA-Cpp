90e416fb11c21e01590d059e51aebe8b9f46ab19
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-08-19 16:57:13 +0530

    Time: 14 ms (100.00%) | Memory: 53.9 MB (26.58%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -1,21 +1,20 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, Set<Integer>> reserved = new HashMap<>();
-        for (int[] seat : reservedSeats) {
-            reserved.computeIfAbsent(seat[0], k -> new HashSet<>()).add(seat[1]);
-	        }
-	    int ans = 0;
-        for (Set<Integer> seats : reserved.values()) {
-            boolean left = true, right = true, middle = true;
-            for (int s : new int[]{2,3,4,5}) if (seats.contains(s)) left = false;
-            for (int s : new int[]{6,7,8,9}) if (seats.contains(s)) right = false;
-            for (int s : new int[]{4,5,6,7}) if (seats.contains(s)) middle = false;
-            if (left && right)
-                ans += 2;
-            else if (left || right || middle)
-            ans += 1;
+        Map<Integer, Integer> d = new HashMap<>();
+        for (var e : reservedSeats) {
+            int i = e[0], j = e[1];
+            d.merge(i, 1 << (10 - j), (x, y) -> x | y);
+        }
+        int[] masks = {0b0111100000, 0b0000011110, 0b0001111000};
+        int ans = (n - d.size()) * 2;
+        for (int x : d.values()) {
+            for (int mask : masks) {
+                if ((x & mask) == 0) {
+                    x |= mask;
+                    ++ans;
+                }
             }
-            ans += 2 * (n - reserved.size());
-            return ans;
-            }
-            }
+        }
+        return ans;
+    }
+}
