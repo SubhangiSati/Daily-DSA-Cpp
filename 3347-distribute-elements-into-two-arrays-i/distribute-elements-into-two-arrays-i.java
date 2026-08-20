8ca1cb745c6bc42f03f4012e48e78aee9731aafe
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-08-20 13:07:09 +0530

    Time: 1 ms (98.08%) | Memory: 46.5 MB (90.87%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,15 +1,27 @@
 class Solution {
-  public int[] resultArray(int[] nums) {
-    List<Integer> arr1 = new ArrayList<>(List.of(nums[0]));
-    List<Integer> arr2 = new ArrayList<>(List.of(nums[1]));
-
-    for (int i = 2; i < nums.length; ++i)
-      if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1))
-        arr1.add(nums[i]);
-      else
-        arr2.add(nums[i]);
-
-    arr1.addAll(arr2);
-    return arr1.stream().mapToInt(Integer::intValue).toArray();
-  }
+    static {
+        for(int i = 0; i <= 500; i++) {
+            resultArray(new int[2]);
+        }
+    }
+    public static int[] resultArray(int[] nums) {
+        int n = nums.length;
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n-1];
+        arr1[0] = nums[0];
+        arr2[0] = nums[1];
+        int arr1Pointer = 0;
+        int arr2Pointer = 0;
+        for(int i = 2; i < n; i++) {
+            if(arr1[arr1Pointer] > arr2[arr2Pointer]) {
+                arr1[++arr1Pointer] = nums[i];
+            } else {
+                arr2[++arr2Pointer] = nums[i];
+            }
+        }
+        for(int i = 0; i <= arr2Pointer; i++) {
+            arr1[++arr1Pointer] = arr2[i];
+        }
+        return arr1;
+    }
 }
