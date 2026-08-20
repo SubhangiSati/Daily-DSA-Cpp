e74cbda5570f8963c5a1c4e78e8eda006d102e09
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-08-20 13:09:14 +0530

    Time: 1 ms (98.08%) | Memory: 46.5 MB (90.87%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -1,27 +1,37 @@
 class Solution {
-    static {
-        for(int i = 0; i <= 500; i++) {
-            resultArray(new int[2]);
-        }
-    }
-    public static int[] resultArray(int[] nums) {
+    public int[] resultArray(int[] nums) {
         int n = nums.length;
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[n-1];
-        arr1[0] = nums[0];
-        arr2[0] = nums[1];
-        int arr1Pointer = 0;
-        int arr2Pointer = 0;
-        for(int i = 2; i < n; i++) {
-            if(arr1[arr1Pointer] > arr2[arr2Pointer]) {
-                arr1[++arr1Pointer] = nums[i];
+        int[] result = new int[n];
+        int arr1End = 0;
+        int arr2Start = n - 1;
+        result[0] = nums[0];       // arr1
+        result[n - 1] = nums[1];   // arr2
+        for (int i = 2; i < n; i++) {
+            if (result[arr1End] > result[arr2Start]) {
+                result[++arr1End] = nums[i];
             } else {
-                arr2[++arr2Pointer] = nums[i];
+                result[--arr2Start] = nums[i];
             }
         }
-        for(int i = 0; i <= arr2Pointer; i++) {
-            arr1[++arr1Pointer] = arr2[i];
+        // arr2 currently goes from right to left,
+        // so reverse it into the final position
+        int left = arr2Start;
+        int right = n - 1;
+        while (left < right) {
+            int temp = result[left];
+            result[left] = result[right];
+            result[right] = temp;
+
+            left++;
+            right--;
         }
-        return arr1;
+        // Move arr2 next to arr1
+        int arr1Length = arr1End + 1;
+        int arr2Length = n - arr1Length;
+        // Shift arr2 to immediately after arr1
+        int[] finalResult = new int[n];
+        System.arraycopy(result, 0, finalResult, 0, arr1Length);
+        System.arraycopy(result, arr1Length, finalResult, arr1Length, arr2Length);
+        return finalResult;
     }
 }
