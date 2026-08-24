e1da753043b48052ba52e549d4a13e87daf392bc
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-08-24 14:48:25 +0530

    Time: 3 ms (100.00%) | Memory: 92.2 MB (37.02%) - LeetSync

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -1,14 +1,15 @@
 class Solution {
-  public int stoneGameVIII(int[] stones) {
-    final int n = stones.length;
-    int[] prefix = stones.clone();
-    int[] dp = new int[n];
-    Arrays.fill(dp, Integer.MIN_VALUE);
-    for (int i = 1; i < prefix.length; ++i)
-      prefix[i] += prefix[i - 1];
-    dp[n - 2] = prefix[n - 1];
-    for (int i = n - 3; i >= 0; --i)
-      dp[i] = Math.max(dp[i + 1], prefix[i + 1] - dp[i + 1]);
-    return dp[0];
-  }
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int prefix[] = new int[n];
+        prefix[0]=stones[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+stones[i];  
+        }
+        int best = prefix[n-1];
+        for(int i=n-2;i>=1;i--){
+            best=Math.max(best,prefix[i]-best);
+        }
+        return best;
+    }
 }
