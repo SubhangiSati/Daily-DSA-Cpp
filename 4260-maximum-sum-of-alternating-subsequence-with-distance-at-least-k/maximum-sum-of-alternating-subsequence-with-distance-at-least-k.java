d4027c6dc27453dae1a9593f9deab04caa67b9e9
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-06 01:42:20 +0530

    Time: 115 ms (100.00%) | Memory: 268.7 MB (67.74%) - LeetSync

--- 4260-maximum-sum-of-alternating-subsequence-with-distance-at-least-k/maximum-sum-of-alternating-subsequence-with-distance-at-least-k.java
+++ 4260-maximum-sum-of-alternating-subsequence-with-distance-at-least-k/maximum-sum-of-alternating-subsequence-with-distance-at-least-k.java
@@ -1,71 +1,50 @@
 class Solution {
     public long maxAlternatingSum(int[] nums, int k) {
-        long maxSum = 0;
         int n = nums.length;
-        int m = Arrays.stream(nums).max().getAsInt();
-        long[][] dp = new long[n][2];
-        SegmentTree[] sts = new SegmentTree[2];
-        for (int j = 0; j < 2; j++) {
-            sts[j] = new SegmentTree(m + 1);
+        int maxVal = 0;
+        for (int num : nums) {
+            if (num > maxVal) 
+                maxVal = num;
         }
+        
+        long[] dp0 = new long[n];
+        long[] dp1 = new long[n];
+        long[] bit0 = new long[maxVal + 2];
+        long[] bit1 = new long[maxVal + 2];
+        long ans = 0;
+
         for (int i = 0; i < n; i++) {
             if (i >= k) {
-                sts[0].update(nums[i - k], dp[i - k][0]);
-                sts[1].update(nums[i - k], dp[i - k][1]);
+                int prev = nums[i - k];
+                update(bit0, maxVal - prev + 1, dp0[i - k]);
+                update(bit1, prev, dp1[i - k]);
             }
-            dp[i][0] = sts[1].getMax(0, nums[i] - 1) + nums[i];
-            dp[i][1] = sts[0].getMax(nums[i] + 1, m) + nums[i];
-            maxSum = Math.max(maxSum, Math.max(dp[i][0], dp[i][1]));
+            
+            int x = nums[i];
+            dp0[i] = x + query(bit1, x - 1);
+            dp1[i] = x + query(bit0, maxVal - x);
+            
+            if (dp0[i] > ans) 
+                ans = dp0[i];
+            if (dp1[i] > ans) 
+                ans = dp1[i];
         }
-        return maxSum;
-    }
-}
-
-class SegmentTree {
-    private int n;
-    private long[] tree;
-
-    public SegmentTree(int n) {
-        this.n = n;
-        this.tree = new long[n * 4];
+        return ans;
     }
 
-    public long getMax(int start, int end) {
-        return getMax(start, end, 0, 0, n - 1);
-    }
-
-    public void update(int index, long value) {
-        update(index, value, 0, 0, n - 1);
-    }
-
-    private long getMax(int rangeStart, int rangeEnd, int treeIndex, int treeStart, int treeEnd) {
-        if (rangeStart > rangeEnd) {
-            return 0;
-        }
-        if (rangeStart == treeStart && rangeEnd == treeEnd) {
-            return tree[treeIndex];
-        }
-        int mid = treeStart + (treeEnd - treeStart) / 2;
-        if (rangeEnd <= mid) {
-            return getMax(rangeStart, rangeEnd, treeIndex * 2 + 1, treeStart, mid);
-        } else if (rangeStart > mid) {
-            return getMax(rangeStart, rangeEnd, treeIndex * 2 + 2, mid + 1, treeEnd);
-        } else {
-            return Math.max(getMax(rangeStart, mid, treeIndex * 2 + 1, treeStart, mid), getMax(mid + 1, rangeEnd, treeIndex * 2 + 2, mid + 1, treeEnd));
+    private void update(long[] bit, int idx, long val) {
+        for (; idx < bit.length; idx += idx & -idx) {
+            if (val > bit[idx]) 
+                bit[idx] = val;
         }
     }
 
-    private void update(int rangeIndex, long value, int treeIndex, int start, int end) {
-        if (start == end) {
-            tree[treeIndex] = value;
-            return;
+    private long query(long[] bit, int idx) {
+        long res = 0;
+        for (; idx > 0; idx -= idx & -idx) {
+            if (bit[idx] > res) 
+                res = bit[idx];
         }
-        int mid = start + (end - start) / 2;
-        if (rangeIndex <= mid) {
-            update(rangeIndex, value, treeIndex * 2 + 1, start, mid);
-        } else {
-            update(rangeIndex, value, treeIndex * 2 + 2, mid + 1, end);
-        }
-        tree[treeIndex] = Math.max(tree[treeIndex * 2 + 1], tree[treeIndex * 2 + 2]);
+        return res;
     }
 }
