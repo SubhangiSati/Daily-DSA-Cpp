41b08f43ddf8037a76ffe985caff59ca9f934465
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-05 22:54:22 +0530

    Time: 90 ms (100.00%) | Memory: 56.8 MB (92.31%) - LeetSync

---
+++ 4153-count-routes-to-climb-a-rectangular-grid/count-routes-to-climb-a-rectangular-grid.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int numberOfRoutes(String[] grid, int d) {
+        int n = grid.length;
+        int m = grid[0].length();
+        long mod = 1_000_000_007;
+        long[] dp = null;
+        for (int i = n - 1; i >= 0; i--) {
+            String r = grid[i];
+            if (dp == null) {
+                long[] init = new long[m];
+                Arrays.fill(init, 1);
+                dp = f(init, 0, r, m, mod);
+            } else {
+                int d2 = (int) Math.sqrt((double) d * d - 1);
+                dp = f(dp, d2, r, m, mod);
+            }
+            dp = f(dp, d, r, m, mod);
+        }
+        long res = 0;
+        for (long v : dp) 
+            res = (res + v) % mod;
+        return (int) (res);
+    }
+
+    private long[] f(long[] dp, int dist, String r, int m, long mod) {
+        long[] dp2 = new long[m];
+        long window = 0;
+        for (int k = 0; k <= Math.min(m - 1, dist); k++) 
+            window += dp[k];
+        dp2[0] = window;
+        for (int j = 1; j < m; j++) {
+            window = dp2[j - 1];
+            if (j - dist - 1 >= 0) 
+                window -= dp[j - dist - 1];
+            if (j + dist < m) 
+                window += dp[j + dist];
+            dp2[j] = window;
+        }
+        for (int j = 0; j < m; j++) 
+            dp2[j] = (r.charAt(j) == '#') ? 0 : dp2[j] % mod;
+        return dp2;
+    }
+}
