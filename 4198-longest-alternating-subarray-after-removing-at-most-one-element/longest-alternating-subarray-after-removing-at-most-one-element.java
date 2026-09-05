0075be455496dc8c46bf2c06419b82d01b2ebe12
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-05 23:38:15 +0530

    Time: 14 ms (91.67%) | Memory: 176.8 MB (75.00%) - LeetSync

--- 4198-longest-alternating-subarray-after-removing-at-most-one-element/longest-alternating-subarray-after-removing-at-most-one-element.java
+++ 4198-longest-alternating-subarray-after-removing-at-most-one-element/longest-alternating-subarray-after-removing-at-most-one-element.java
@@ -1,43 +1,55 @@
 class Solution {
     public int longestAlternating(int[] nums) {
-        int n = nums.length;
-        int[] l1 = new int[n];
-        int[] l2 = new int[n];
-        int[] r1 = new int[n];
-        int[] r2 = new int[n];
+        int l, r; l = 0; r = 1;
+        Boolean up, used; up = null; used = false;
+        int maxx = 0; int usedIdx = 0;
 
-        for (int i = 0; i < n; i++) {
-            l1[i] = 1;
-            l2[i] = 1;
-            r1[i] = 1;
-            r2[i] = 1;
+        while (r <= nums.length - 1) {
+            int n1, n2; n1 = nums[r-1]; n2 = nums[r];
+            if (n1 < n2) {
+                if(up != null && up) {
+                    if (used) {
+                        used = false;
+                        l = usedIdx;
+                    } else { 
+                        used = true;
+                        usedIdx = r-1;
+                        maxx = Math.max(maxx, r-l);
+                        r++;
+                    }
+                } else { 
+                    maxx = used ? Math.max(maxx, r-l) : Math.max(maxx, r-l+1);
+                    r++;
+                }
+                up = true;
+            } else if (n1 > n2) {
+                if(up != null && !up) {
+                   if (used) {
+                        used = false;
+                        l = usedIdx;
+                    } else {
+                        used = true;
+                        usedIdx = r-1;
+                        maxx = Math.max(maxx, r-l);
+                        r++;
+                    }
+                } else {
+                    maxx = used ? Math.max(maxx, r-l) : Math.max(maxx, r-l+1);
+                    r++;
+                }
+                up = false;
+            } else{
+                if (used) {
+                    used = false;
+                    l = usedIdx;
+                } else {
+                    used = true;
+                    usedIdx = r;
+                    maxx = Math.max(maxx, r-l);
+                    r++;
+                }
+            }
         }
-
-        int ans = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i]) 
-                l1[i] = l2[i - 1] + 1;
-            else if (nums[i - 1] > nums[i]) 
-                l2[i] = l1[i - 1] + 1;
-            ans = Math.max(ans, l1[i]);
-            ans = Math.max(ans, l2[i]);
-        }
-
-        for (int i = n - 2; i >= 0; i--){ 
-            if (nums[i + 1] > nums[i]) 
-                r1[i] = r2[i + 1] + 1;
-            else if (nums[i + 1] < nums[i]) 
-                r2[i] = r1[i + 1] + 1;
-        }
-
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i - 1] < nums[i + 1]) 
-                ans = Math.max(ans, l2[i - 1] + r2[i + 1]);
-            else if (nums[i - 1] > nums[i + 1]) 
-                ans = Math.max(ans, l1[i - 1] + r1[i + 1]);
-        }
-
-        return ans;
+        return maxx;
     }
 }
