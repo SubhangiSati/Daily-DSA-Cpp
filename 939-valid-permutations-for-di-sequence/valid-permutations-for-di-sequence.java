fb2ae8889aa859f0c967f204ff2b37a73296d882
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-07 17:03:16 +0530

    Time: 4 ms (42.78%) | Memory: 43.7 MB (56.67%) - LeetSync

--- 939-valid-permutations-for-di-sequence/valid-permutations-for-di-sequence.java
+++ 939-valid-permutations-for-di-sequence/valid-permutations-for-di-sequence.java
@@ -1,25 +1,32 @@
 class Solution {
     public int numPermsDISequence(String s) {
-        final int mod = (int) 1e9 + 7;
+        int MOD = 1_000_000_007;
         int n = s.length();
-        int[][] f = new int[n + 1][n + 1];
-        f[0][0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            if (s.charAt(i - 1) == 'D') {
-                for (int j = 0; j <= i; ++j) {
-                    for (int k = j; k < i; ++k)
-                        f[i][j] = (f[i][j] + f[i - 1][k]) % mod;
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            long[] newDp = new long[n + 1];
+            if (s.charAt(i) == 'I') {
+                long sum = 0;
+                for (int j = 0; j <= i + 1; j++) {
+                    newDp[j] = sum;
+                    if (j <= i) 
+                        sum = (sum + dp[j]) % MOD;
                 }
-            } else {
-                for (int j = 0; j <= i; ++j) {
-                    for (int k = 0; k < j; ++k)
-                        f[i][j] = (f[i][j] + f[i - 1][k]) % mod;
+            } 
+            else {
+                long sum = 0;
+                for (int j = i; j >= 0; j--) {
+                    sum = (sum + dp[j]) % MOD;
+                    newDp[j] = sum;
                 }
             }
+            dp = newDp;
         }
-        int ans = 0;
-        for (int j = 0; j <= n; ++j)
-            ans = (ans + f[n][j]) % mod;
-        return ans;
+        long answer = 0;
+        for (int i = 0; i <= n; i++) 
+            answer = (answer + dp[i]) % MOD;
+        return (int) answer;
     }
 }
