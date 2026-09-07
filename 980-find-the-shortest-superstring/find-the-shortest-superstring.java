8eb9761a2aad782a74f50a4e973d90324c902db1
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-07 17:33:43 +0530

    Time: 21 ms (99.04%) | Memory: 47 MB (88.94%) - LeetSync

--- 980-find-the-shortest-superstring/find-the-shortest-superstring.java
+++ 980-find-the-shortest-superstring/find-the-shortest-superstring.java
@@ -1,63 +1,74 @@
 class Solution {
     public String shortestSuperstring(String[] words) {
         int n = words.length;
-        int[][] g = new int[n][n];
-        for (int i = 0; i < n; ++i) {
-            String a = words[i];
-            for (int j = 0; j < n; ++j) {
-                String b = words[j];
-                if (i != j) {
-                    for (int k = Math.min(a.length(), b.length()); k > 0; --k) {
-                        if (a.substring(a.length() - k).equals(b.substring(0, k))) {
-                            g[i][j] = k;
-                            break;
-                        }
+        int[][] overlap = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+                int max = Math.min(words[i].length(), words[j].length());
+                for (int k = max; k >= 0; k--) {
+                    if (words[i].endsWith(words[j].substring(0, k))) {
+                        overlap[i][j] = k;
+                        break;
                     }
                 }
             }
         }
-        int[][] dp = new int[1 << n][n];
-        int[][] p = new int[1 << n][n];
-        for (int i = 0; i < 1 << n; ++i) {
-            Arrays.fill(p[i], -1);
-            for (int j = 0; j < n; ++j) {
-                if (((i >> j) & 1) == 1) {
-                    int pi = i ^ (1 << j);
-                    for (int k = 0; k < n; ++k) {
-                        if (((pi >> k) & 1) == 1) {
-                            int v = dp[pi][k] + g[k][j];
-                            if (v > dp[i][j]) {
-                                dp[i][j] = v;
-                                p[i][j] = k;
-                            }
-                        }
-                    }
+        int size = 1 << n;
+        int[][] dp = new int[size][n];
+        for (int i = 0; i < size; i++)
+            Arrays.fill(dp[i], -1);
+        for (int i = 0; i < n; i++)
+            dp[1 << i][i] = 0;
+        for (int mask = 1; mask < size; mask++) {
+            for (int last = 0; last < n; last++) {
+                if (dp[mask][last] == -1)
+                    continue;
+
+                for (int next = 0; next < n; next++) {
+                    if ((mask & (1 << next)) != 0)
+                        continue;
+
+                    int nm = mask | (1 << next);
+                    dp[nm][next] = Math.max(
+                            dp[nm][next],
+                            dp[mask][last] + overlap[last][next]);
                 }
             }
         }
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (dp[(1 << n) - 1][i] > dp[(1 << n) - 1][j]) 
-                j = i;
+
+        int full = size - 1;
+        int last = 0;
+        for (int i = 1; i < n; i++) {
+            if (dp[full][i] > dp[full][last])
+                last = i;
         }
-        List<Integer> arr = new ArrayList<>();
-        arr.add(j);
-        for (int i = (1 << n) - 1; p[i][j] != -1;) {
-            int k = i;
-            i ^= (1 << j);
-            j = p[k][j];
-            arr.add(j);
+        List<Integer> order = new java.util.ArrayList<>();
+        int mask = full;
+        while (mask != 0) {
+            order.add(last);
+            int prev = -1;
+            for (int p = 0; p < n; p++) {
+                if (p == last || (mask & (1 << p)) == 0)
+                    continue;
+                if (dp[mask ^ (1 << last)][p] != -1 &&
+                        dp[mask][last] == dp[mask ^ (1 << last)][p] + overlap[p][last]) {
+                    prev = p;
+                    break;
+                }
+            }
+            mask ^= 1 << last;
+            if (mask == 0)
+                break;
+            last = prev;
         }
-        Set<Integer> vis = new HashSet<>(arr);
-        for (int i = 0; i < n; ++i) {
-            if (!vis.contains(i)) 
-                arr.add(i);
-        }
-        Collections.reverse(arr);
-        StringBuilder ans = new StringBuilder(words[arr.get(0)]);
-        for (int i = 1; i < n; ++i) {
-            int k = g[arr.get(i - 1)][arr.get(i)];
-            ans.append(words[arr.get(i)].substring(k));
+        Collections.reverse(order);
+        StringBuilder ans = new StringBuilder(words[order.get(0)]);
+        for (int i = 1; i < order.size(); i++) {
+            int a = order.get(i - 1);
+            int b = order.get(i);
+            ans.append(words[b].substring(overlap[a][b]));
         }
         return ans.toString();
     }
