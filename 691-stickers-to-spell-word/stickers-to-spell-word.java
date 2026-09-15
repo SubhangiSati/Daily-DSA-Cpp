ba382335f2bc31e4e0efa3eacb24589ab1f33179
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-15 11:58:47 +0530

    Time: 6 ms (100.00%) | Memory: 46.7 MB (81.77%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,34 +1,56 @@
 class Solution {
+    Integer[] dp;
     public int minStickers(String[] stickers, String target) {
-        int n = target.length();
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(0);
-        boolean[] vis = new boolean[1 << n];
-        vis[0] = true;
-        for (int ans = 0; !q.isEmpty(); ++ans) {
-            for (int m = q.size(); m > 0; --m) {
-                int curr = q.poll();
-                if (curr == (1 << n) - 1) 
-                    return ans;
-                for (String s : stickers) {
-                    int[] count = new int[26];
-                    int next = curr;
-                    for (char c : s.toCharArray()) 
-                        ++count[c - 'a'];
-                    for (int i = 0; i < n; ++i) {
-                        int j = target.charAt(i) - 'a';
-                        if ((curr >> i & 1) == 0 && count[j] > 0) {
-                            --count[j];
-                            next |= 1 << i;
-                        }
-                    }
-                    if (!vis[next]) {
-                        vis[next] = true;
-                        q.offer(next);
+        int n = stickers.length;
+        int[][] freq = new int[n][26];
+        for(int i=0;i<n;i++){
+            for(char ch : stickers[i].toCharArray()){
+                freq[i][ch-'a']++;
+            }
+        }
+
+        dp = new Integer[1<<target.length()];
+        int targetBitMask = (1<<target.length()) - 1;
+        int ans =  helper(freq, target, targetBitMask, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private int helper(int[][] freq, String target, int targetMask, int currMask){
+        if(currMask == targetMask) return 0;
+        if(dp[currMask] != null) return dp[currMask];
+
+        int firstUncovered = -1;
+        for (int i = 0; i < target.length(); i++) {
+            if ((currMask & (1 << i)) == 0) {
+                firstUncovered = i;
+                break;
+            }
+        }
+
+        int res = Integer.MAX_VALUE;
+        char targetChar = target.charAt(firstUncovered);
+
+        for(int i=0;i<freq.length;i++){
+            if (freq[i][targetChar - 'a'] == 0) continue;
+            
+            int[] tempFreq = new int[26];
+            int newMask = currMask;
+            
+            for(int b=0;b<target.length();b++){
+                if((newMask & (1<<b)) == 0){
+                    int t = target.charAt(b)-'a';
+                    if(tempFreq[t] < freq[i][t]){
+                        tempFreq[t]++;
+                        newMask |= (1<<b);
                     }
                 }
             }
+
+            int subProblem = helper(freq, target, targetMask, newMask);
+            if (subProblem != Integer.MAX_VALUE) 
+                res = Math.min(res, 1 + subProblem);
         }
-        return -1;
+
+        return dp[currMask] = res;
     }
 }
