1a04e82e32451d2e19f609fb4ba63621bb2c4e17
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-16 13:16:31 +0530

    Time: 31 ms (100.00%) | Memory: 80.3 MB (85.29%) - LeetSync

--- 3153-apply-operations-on-array-to-maximize-sum-of-squares/apply-operations-on-array-to-maximize-sum-of-squares.java
+++ 3153-apply-operations-on-array-to-maximize-sum-of-squares/apply-operations-on-array-to-maximize-sum-of-squares.java
@@ -1,23 +1,20 @@
 class Solution {
     public int maxSum(List<Integer> nums, int k) {
-        final int mod = (int) 1e9 + 7;
-        int[] count = new int[31];
-        for (int a : nums) {
-            for (int i = 0; i < 31; ++i) {
-                if ((a >> i & 1) == 1) 
-                    ++count[i];
-            }
+        int[] count = new int[32];
+        int mod = (int) (1e9 + 7);
+        for (int num : nums){
+            for (int i = 0; i < 32; ++i)
+                count[i] += num >> i & 1;   
         }
         long ans = 0;
-        while (k-- > 0) {
-            int a = 0;
-            for (int i = 0; i < 31; ++i) {
-                if (count[i] > 0) {
-                    a |= 1 << i;
-                    --count[i];
-                }
+        for (int i = 0; i < k; ++i){
+            int c = 0;
+            for (int j = 0; j < 32; ++j){
+                if (count[j] == 0) continue;
+                c |= 1 << j;
+                --count[j];
             }
-            ans = (ans + 1L * a * a) % mod;
+            ans = (ans + 1L * c * c) % mod;
         }
         return (int) ans;
     }
