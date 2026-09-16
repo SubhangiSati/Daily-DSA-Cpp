7edc1948b873906fe60c079135f4501e5db787ab
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-16 11:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (79.35%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int numberOfSets(int n, int k) {
-        final int mod = (int) 1e9 + 7;
-        int[][] p = new int[n + 1][k + 1];
-        int[][] q = new int[n + 1][k + 1];
-        p[1][0] = 1;
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 0; j <= k; ++j) {
-                p[i][j] = (p[i - 1][j] + q[i - 1][j]) % mod;
-                q[i][j] = q[i - 1][j];
-                if (j > 0) {
-                    q[i][j] = (q[i][j] + p[i - 1][j - 1]) % mod;
-                    q[i][j] = (q[i][j] + q[i - 1][j - 1]) % mod;
-                }
-            }
+    private static final long MOD = 1000000007L;
+    private long quickPow(long a, long b) {
+        long result = 1;
+        while (b > 0) {
+            if ((b & 1) != 0) result = (result * a) % MOD;
+            a = (a * a) % MOD;
+            b >>= 1;
         }
-        return (p[n][k] + q[n][k]) % mod;
+        return result;
+    }
+
+    public int numberOfSets(int n, int k) {
+        int m = 2 * k;
+        long num = 1, deno = 1;
+        for (int i = 1; i <= m; i++) {
+            num = (num * (n + k - i)) % MOD;
+            deno = (deno * i) % MOD;
+        }
+        return (int) ((num * quickPow(deno, MOD - 2)) % MOD);
     }
 }
