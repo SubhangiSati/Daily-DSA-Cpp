fd740c9e326c4157955e3f975f3d8aa1b80f0d82
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-17 12:51:56 +0530

    Time: 59 ms (89.02%) | Memory: 190.3 MB (90.24%) - LeetSync

--- 2652-count-number-of-possible-root-nodes/count-number-of-possible-root-nodes.java
+++ 2652-count-number-of-possible-root-nodes/count-number-of-possible-root-nodes.java
@@ -1,55 +1,33 @@
 class Solution {
-    private List<Integer>[] g;
-    private Map<Long, Integer> gs = new HashMap<>();
-    private int ans;
-    private int k;
-    private int count;
-    private int n;
-
-    public int rootCount(int[][] edges, int[][] guesses, int k) {
-        this.k = k;
-        n = edges.length + 1;
-        g = new List[n];
-        Arrays.setAll(g, e -> new ArrayList<>());
-        for (var e : edges) {
-            int a = e[0], b = e[1];
-            g[a].add(b);
-            g[b].add(a);
+    public int rootCount(int[][] eg, int[][] gu, int k) {
+        int n = eg.length+1;
+        List<Integer>[] g = new List[n];
+        for(int i = 0; i<n; i++) g[i] = new LinkedList<>();
+        for(int[] a: eg){
+            g[a[0]].add(a[1]);
+            g[a[1]].add(a[0]);
         }
-        for (var e : guesses) {
-            int a = e[0], b = e[1];
-            gs.merge(f(a, b), 1, Integer::sum);
-        }
-        dfs1(0, -1);
-        dfs2(0, -1);
+        int all = 0;
+        int[] add = new int[n], parent = new int[n];
+        dfs1(g, 0, -1, parent);
+        for(int[] a: gu) if(parent[a[1]] == a[0]){
+            all++;
+            add[a[1]]--;
+        } else 
+        add[a[0]]++;
+        dfs2(g, 0, -1, add);
+        int ans = 0;
+        for(int i: add) if(i + all >= k) ans++;
         return ans;
     }
-
-    private void dfs1(int i, int fa) {
-        for (int j : g[i]) {
-            if (j != fa) {
-                count += gs.getOrDefault(f(i, j), 0);
-                dfs1(j, i);
-            }
-        }
+    private void dfs1(List<Integer>[] g, int s, int p, int[] parent){
+        parent[s] = p;
+        for(int i: g[s]) if(i != p) dfs1(g, i, s, parent);
     }
-
-    private void dfs2(int i, int f) {
-        ans += count >= k ? 1 : 0;
-        for (int j : g[i]) {
-            if (j != f) {
-                int a = gs.getOrDefault(f(i, j), 0);
-                int b = gs.getOrDefault(f(j, i), 0);
-                count -= a;
-                count += b;
-                dfs2(j, i);
-                count -= b;
-                count += a;
-            }
+    private void dfs2(List<Integer>[] g, int s, int p, int[] add){
+        for(int i: g[s]) if(i != p){
+            add[i]+=add[s];
+            dfs2(g, i, s, add);
         }
-    }
-
-    private long f(int i, int j){
-        return 1L * i * n + j;
     }
 }
