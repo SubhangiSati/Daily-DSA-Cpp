d26a039a54a4662df056bad2efb91fc37e443a24
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-17 15:07:53 +0530

    Time: 12 ms (100.00%) | Memory: 114.8 MB (34.58%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -1,18 +1,19 @@
 class Solution {
     public int constrainedSubsetSum(int[] nums, int k) {
-        Deque<Integer> q = new ArrayDeque<>();
-        q.offer(0);
         int n = nums.length;
-        int[] f = new int[n];
-        int ans = -(1 << 30);
-        for (int i = 0; i < n; ++i) {
-            while (i - q.peekFirst() > k) 
-                q.pollFirst();
-            f[i] = Math.max(0, f[q.peekFirst()]) + nums[i];
-            ans = Math.max(ans, f[i]);
-            while (!q.isEmpty() && f[q.peekLast()] <= f[i]) 
-                q.pollLast();
-            q.offerLast(i);
+        int[] deque = new int[n];
+        int front = 0;
+        int rear = 0;
+        int ans = nums[0];
+        for (int i = 0; i < n; i++) {
+            while (front < rear && deque[front] < i - k)
+                front++;
+            if (front < rear && nums[deque[front]] > 0)
+                nums[i] += nums[deque[front]];
+            ans = Math.max(ans, nums[i]);
+            while (front < rear && nums[deque[rear - 1]] <= nums[i])
+                rear--;
+            deque[rear++] = i;
         }
         return ans;
     }
