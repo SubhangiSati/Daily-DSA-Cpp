aa135246933ea1996753428b9c6ff9b5559907c1
Subhangi Sati <113504623+SubhangiSati@users.noreply.github.com>
2026-09-25 18:14:57 +0530

    Time: 7 ms (99.20%) | Memory: 46.8 MB (90.40%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.java
+++ 1188-brace-expansion-ii/brace-expansion-ii.java
@@ -1,21 +1,44 @@
 class Solution {
-    private TreeSet<String> s = new TreeSet<>();
+  public List<String> braceExpansionII(String expression) {
+    return dfs(expression, 0, expression.length() - 1);
+  }
 
-    public List<String> braceExpansionII(String expression) {
-        dfs(expression);
-        return new ArrayList<>(s);
+  private List<String> dfs(final String expression, int s, int e) {
+    TreeSet<String> ans = new TreeSet<>();
+    List<List<String>> groups = new ArrayList<>();
+    groups.add(new ArrayList<>());
+    int layer = 0;
+    int left = 0;
+
+    for (int i = s; i <= e; ++i)
+      if (expression.charAt(i) == '{' && ++layer == 1)
+        left = i + 1;
+      else if (expression.charAt(i) == '}' && --layer == 0)
+        merge(groups, dfs(expression, left, i - 1));
+      else if (expression.charAt(i) == ',' && layer == 0)
+        groups.add(new ArrayList<>());
+      else if (layer == 0)
+        merge(groups, new ArrayList<>(List.of(String.valueOf(expression.charAt(i)))));
+
+    for (final List<String> group : groups)
+      for (final String word : group)
+        ans.add(word);
+
+    return new ArrayList<>(ans);
+  }
+
+  void merge(List<List<String>> groups, List<String> group) {
+    if (groups.get(groups.size() - 1).isEmpty()) {
+      groups.set(groups.size() - 1, group);
+      return;
     }
 
-    private void dfs(String exp) {
-        int j = exp.indexOf('}');
-        if (j == -1) {
-            s.add(exp);
-            return;
-        }
-        int i = exp.lastIndexOf('{', j);
-        String a = exp.substring(0, i);
-        String c = exp.substring(j + 1);
-        for (String b : exp.substring(i + 1, j).split(",")) 
-            dfs(a + b + c);
-    }
+    List<String> mergedGroup = new ArrayList<>();
+
+    for (final String word1 : groups.get(groups.size() - 1))
+      for (final String word2 : group)
+        mergedGroup.add(word1 + word2);
+
+    groups.set(groups.size() - 1, mergedGroup);
+  }
 }
